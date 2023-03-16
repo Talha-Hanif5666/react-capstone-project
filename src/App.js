2965aa8280434e7e1924e78433429c703fcbe648
Muhammad Talha Hanif <talhahanif5666@gmail.com>
2023-03-16 16:42:56 +0500

    Adding App.js and Index.js files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Details from './components/countrydetail';
+import Home from './components/home';
+import './index.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/:countryId" element={<Details />} />
+      </Routes>
     </div>
   );
 }
